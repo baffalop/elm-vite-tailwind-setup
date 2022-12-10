40e71c29fe37bc08c4d2022beef257b8d7975c0f
Nikita Gaidakov <ngaidakov@podfather.com>
2022-12-10 21:25:52 +0000

    Code style my way

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  content: ["./index.html", "./main.js", "./src/**/*.elm"],
+  content: ['./index.html', './main.js', './src/**/*.elm'],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
